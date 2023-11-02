9f4cca0ae4cdcfc9b56e1c0449906eb5b373f4fc
FloresDavid <Davidabelflores@gmail.com>
2023-11-02 12:38:27 -0300

    feat: profile.html

--- app.js
+++ app.js
@@ -9,3 +9,4 @@ app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${POR
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
 app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
 app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+app.get('/profile',(req,res) => res.sendFile(path.join(__dirname,'views','profile.html')));
