ff43647e15455aba35a6df6079b23997447eb37e
FloresDavid <Davidabelflores@gmail.com>
2023-11-21 10:35:34 -0300

    add:carrousel

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const PORT = 3030;
+const PORT = 3031;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
